c7f3ffa0a9dff763ce4160f939bb6e4a12d809f2
Tupax <tupaxmaro@gmail.com>
2018-12-04 12:29:26 -0300

    Initial Commit

---
+++ Mgras.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Mgras.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mgras.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ Mgras.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace Mgras.API.Models
+{
+    public class Value
+    {
+            public int Id {get;set;}
+            public string Name { get; set; }
+    }
+}

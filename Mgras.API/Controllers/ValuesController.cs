6cea0f40a8d84d9014dd778cb04b37fe32fd2ac1
Tupax <tupaxmaro@gmail.com>
2018-12-04 22:51:25 -0300

    Auth Users

--- Mgras.API/Controllers/ValuesController.cs
+++ Mgras.API/Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Mgras.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mgras.Controllers
-{
+{   
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase

436dbcad595b3d695f0d5e994732f28084e15796
Tupax <tupaxmaro@gmail.com>
2018-12-05 14:31:29 -0300

    b4 migration

--- Mgras-SPA/src/app/home/home.component.ts
+++ Mgras-SPA/src/app/home/home.component.ts
@@ -2,13 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class ValueComponent implements OnInit {
-
-  // js var parecido
+export class HomeComponent implements OnInit {
+  registerMode = false;
   values: any;
 
   constructor(private http: HttpClient) { }
@@ -17,6 +16,11 @@ export class ValueComponent implements OnInit {
     this.getValues();
   }
 
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+
   getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(response =>{
       this.values = response;
@@ -24,5 +28,4 @@ export class ValueComponent implements OnInit {
       console.log(error);
     });
   }
-
 }
--- Mgras-SPA/src/app/nav/nav.component.ts
+++ Mgras-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +10,28 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authsService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
+    this.authsService.login(this.model).subscribe(next => {
+      console.log('Log OK');
+    }, error => {
+      console.log ('Log FAILD');
+    });
     console.log(this.model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('log out');
   }
 
 }
